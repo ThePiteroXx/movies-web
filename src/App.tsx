0709865026b21daa15d538decc4781780d76e9a0
ThePiteroXx <thepremiumxx@wp.pl>
2022-06-06 15:04:56 +0200

    refractor: replace div tag to section tag

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { CategoryQuery } from 'types';
 
 const App = () => {
   return (
-    <div className='app hide-scroll'>
-      <RowMovies fetchMovie={api.getPopularMovies} queryKey={CategoryQuery.POPULAR} main />
-      <RowMovies fetchMovie={api.getTopRatedMovies} queryKey={CategoryQuery.RATED} title='top rated movies' />
-      <RowMovies fetchMovie={api.getActionMovies} queryKey={CategoryQuery.ACTION} title='action movies' />
-      <RowMovies fetchMovie={api.getHorrorMovies} queryKey={CategoryQuery.HORROR} title='horror movies' />
-      <RowMovies fetchMovie={api.getRomanceMovies} queryKey={CategoryQuery.ROMANCE} title='romance movies' />
+    <div className='app'>
+      <section>
+        <RowMovies fetchMovie={api.getPopularMovies} queryKey={CategoryQuery.POPULAR} main />
+        <RowMovies fetchMovie={api.getTopRatedMovies} queryKey={CategoryQuery.RATED} title='top rated movies' />
+        <RowMovies fetchMovie={api.getActionMovies} queryKey={CategoryQuery.ACTION} title='action movies' />
+        <RowMovies fetchMovie={api.getHorrorMovies} queryKey={CategoryQuery.HORROR} title='horror movies' />
+        <RowMovies fetchMovie={api.getRomanceMovies} queryKey={CategoryQuery.ROMANCE} title='romance movies' />
+      </section>
     </div>
   );
 };
