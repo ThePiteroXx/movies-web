88c8ab6e29eb4ca3a8f2af8957c8a4f0bfbc232c
ThePiteroXx <thepremiumxx@wp.pl>
2022-06-07 02:41:20 +0200

    refractor: modify function and add new class

--- src/components/RowMovies/MovieItem.tsx
+++ src/components/RowMovies/MovieItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useMovie } from 'context/MovieContext';
 import { Movie } from 'types';
 
 type PropsType = {
@@ -7,11 +8,18 @@ type PropsType = {
 };
 
 const MovieItem: React.FC<PropsType> = ({ movie, main }) => {
+  const { setMovie } = useMovie();
   return (
     <div
-      className={`p-1 sm:p-2 shrink-0 ${
+      className={`p-1 sm:p-2 lg:p-4 shrink-0 ${
         main ? 'w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-[12.5%] 2xl:w-[10%]' : 'w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6'
       } text-slate-400`}
+      role='button'
+      tabIndex={-1}
+      onClick={() => setMovie(movie)}
+      onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'Enter') setMovie(movie);
+      }}
     >
       <img
         draggable='false'
--- src/components/RowMovies/SkeletonSlider.tsx
+++ src/components/RowMovies/SkeletonSlider.tsx
@@ -5,7 +5,7 @@ const SkeletonSlider: React.FC<{ main?: boolean }> = ({ main }) => {
     <div className={`w-full flex ${main ? 'items-center' : 'mt-8 flex-start'}`}>
       {elements.map((element) => (
         <div
-          className={`p-1 sm:p-2 shrink-0 ${
+          className={`p-1 sm:p-2 lg:p-4 shrink-0 ${
             main ? 'w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-[12.5%] 2xl:w-[10%]' : 'w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6'
           }`}
           key={element}
