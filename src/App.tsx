30f9374d2d891705dbedfba1e4ae742843f6feea
ThePiteroXx <thepremiumxx@wp.pl>
2022-06-07 02:43:39 +0200

    feat: add header component and classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import * as api from 'api/api';
-import RowMovies from 'components/RowMovies/RowMovies';
 import { CategoryQuery } from 'types';
+
+import RowMovies from 'components/RowMovies/RowMovies';
+import Header from 'components/Header/Header';
 
 const App = () => {
   return (
-    <div className='app'>
-      <section>
+    <div className='app font-sans'>
+      <Header />
+      <section className='lg:mt-[-280px]'>
         <RowMovies fetchMovie={api.getPopularMovies} queryKey={CategoryQuery.POPULAR} main />
         <RowMovies fetchMovie={api.getTopRatedMovies} queryKey={CategoryQuery.RATED} title='top rated movies' />
         <RowMovies fetchMovie={api.getActionMovies} queryKey={CategoryQuery.ACTION} title='action movies' />
