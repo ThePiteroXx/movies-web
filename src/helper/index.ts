88b1f32944e4269e1f702a79c02fabb01bac814d
ThePiteroXx <thepremiumxx@wp.pl>
2022-06-11 23:34:38 +0200

    feat: create helper function

---
+++ src/helper/index.ts
@@ -0,0 +1,10 @@
+export const debounce = (cb: (...args: any) => void, delay = 250) => {
+  let timeout: NodeJS.Timeout;
+
+  return (...args: any) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+};
